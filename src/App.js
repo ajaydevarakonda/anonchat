5eaf82ec3322fed3e52db49c85f8c1096f83f6c1
Edmund Dantes <d.ajaykumar.017@gmail.com>
2019-08-02 11:07:13 +0530

    Add simple routing to the app.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Join from './Join';
+import Chat from './Chat';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  render() { return (<div>Hello world!</div>) }
+  render() {
+    return (<Router>
+      <Switch>
+        <Route exact path="/" render={(routeProps) => (
+          <Join {...routeProps}/>
+        )} />
+        <Route exact path="/chat" render={(routeProps) => (
+          <Chat {...routeProps}/>
+        )} />
+      </Switch>
+      
+    </Router>)
+  }
 }
 
 export default App;
---
+++ src/Join.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Join extends Component {
+    render() {
+        return(<div>Join page</div>);
+    }
+}
+
+export default Join;
