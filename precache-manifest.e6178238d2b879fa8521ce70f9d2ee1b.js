0ff50c9b81ebdac6d86f2567a592c75cc9f37898
Edmund Dantes <d.ajaykumar.017@gmail.com>
2019-08-10 14:43:38 +0530

    Updates

---
+++ precache-manifest.e6178238d2b879fa8521ce70f9d2ee1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9dfb473c52ba8f924fe5c5f19209cb",
+    "url": "/anonchat/index.html"
+  },
+  {
+    "revision": "622478d0c1ffb10fc2e6",
+    "url": "/anonchat/static/css/main.81baa2f4.chunk.css"
+  },
+  {
+    "revision": "f90a7ea3ad87ff05c2e0",
+    "url": "/anonchat/static/js/2.01b935ce.chunk.js"
+  },
+  {
+    "revision": "622478d0c1ffb10fc2e6",
+    "url": "/anonchat/static/js/main.37ffc351.chunk.js"
+  },
+  {
+    "revision": "5a0600dac53d0b7bb136",
+    "url": "/anonchat/static/js/runtime~main.303eabcb.js"
+  }
+]);
